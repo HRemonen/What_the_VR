5aa67135db25221088db6b040d62136d1c03a797
Henri Remonen <henri@remonen.fi>
2023-01-29 15:21:51 +0200

    Isolate Map rendering to component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,34 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useRef, useEffect, useState } from 'react'
-import mapboxgl from 'mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
+import React from 'react'
+import RenderMapBox from './components/map/RenderMapBox'
 import './App.css'
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
-
-const App = () => {
-  const mapContainer = useRef(null)
-  const map = useRef(null)
-  const [lat, setLat] = useState(60.192059)
-  const [lng, setLng] = useState(24.945831)
-  const [zoom, setZoom] = useState(9)
-
-  useEffect(() => {
-    if (map.current) return // initialize map only once
-    console.log(
-      `Map reloaded to position (lat, lng): (${lat}, ${lng}), zoom: ${zoom}`
-    )
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom,
-    })
-  })
-  return (
-    <div>
-      <div ref={mapContainer} className="map-container" />
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <RenderMapBox />
+  </div>
+)
 
 export default App
